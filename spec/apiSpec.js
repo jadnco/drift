670096c5410af8193761568495686216ab2fd0e2
Jaden Dessureault <jaden.dessureault@gmail.com>
2015-10-26 15:24:06 -0500

    Add extra jasmine method library

--- spec/apiSpec.js
+++ spec/apiSpec.js
@@ -1,15 +1,23 @@
 'use strict';
 
+// Add extra jasmine methods
+require('jasmine-expect');
+
 let request = require('request');
 
-let local = 'http://localhost:3000/api/';
+let API = {
+  // Local API address
+  local: 'http://localhost:3000/api/',
+  // External address, available across the network
+  external: ''
+};
 
 describe('Slideshow API', () => {
 
   // This route doesn't exist, we should get a 404
   describe('GET /api', () => {
     it('returns status code 404', (done) => {
-      request.get(local, (error, res, body) => {
+      request.get(API.local, (error, res, body) => {
         expect(res.statusCode).toBe(404);
 
         done();
@@ -17,28 +25,42 @@ describe('Slideshow API', () => {
     });
   });
 
-  describe('GET /api/slideshows', () => {
-    it('returns list of slideshow records', (done) => {
-      request.get(local + 'slideshows', (error, res, body) => {
-        // Body is a string, so convert to a JSON object
+  // Single slideshow record
+  describe('GET /api/slideshow/:token', () => {
+    it('returns a single slideshow record', (done) => {
+      request.get(API.local + 'slideshow/9846', (error, res, body) => {
         body = JSON.parse(body);
 
-        // Check to make sure we are getting at least an empty object
-        expect(body).toEqual(jasmine.any(Object));
+        console.log(body);
+
+        expect(body).toBeObject();
+
+        // Check if .slideshow exists
+        expect(body).toHaveArray('slideshow');
+
+        // Array should only have a single record
+        expect(body.slideshow.length).toBe(1);
 
         done();
       });
     });
   });
 
-  describe('GET /api/slideshow/:token', () => {
-    it('returns a single slideshow record', (done) => {
-      request.get(local + 'slideshow/9846', (error, res, body) => {
+  // List of all slideshow records
+  describe('GET /api/slideshows', () => {
+    it('returns a list of all slideshow records', (done) => {
+      request.get(API.local + 'slideshows', (error, res, body) => {
         body = JSON.parse(body);
-        console.log(res);
-        done();
 
-        //expect(body).toEqual();
+        // Check body for an object
+        expect(body).toBeObject();
+
+        // Check if .slideshows exists
+        expect(body).toHaveArray('slideshows');
+
+        expect(body.slideshows).toBeArrayOfObjects();
+
+        done();
       });
     });
   });
